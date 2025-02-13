29bfa7a6b0d0beca20a7cf06129af22c34247472
Dylan Sieren <dylan.sieren01@gmail.com>
2025-02-13 15:28:45 -0700

    Some login changes since signup isnt needed now

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState } from "react";
 import Typography from "../components/global/Typography";
 import axios from "axios";
@@ -7,23 +7,29 @@ const Login = () => {
   const [email, setEmail] = useState<string>("");
   const [password, setPassword] = useState<string>("");
 
-  const handleUserSignup = (e: React.ChangeEvent<HTMLFormElement>) => {
+  const handleUserLogin = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
-    axios.post('http://localhost:5167/api/login', {email, password});
-  }
+    axios.post("http://localhost:5167/api/login", {
+      email,
+      password,
+    });
+  };
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
-  }  
+  };
   const handlePasswordChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setPassword(e.target.value);
-  }
+  };
   return (
     <main className="container">
       <div className="p-5 bg-white mx-auto my-20 w-1/3  rounded-xl border border-black p-5">
         <Typography variant="h3" className="text-center">
           Admin Login
         </Typography>
-        <form className="flex justify-center items-center flex-col" onSubmit={handleUserSignup}>
+        <form
+          className="flex justify-center items-center flex-col"
+          onSubmit={handleUserLogin}
+        >
           <div className="flex flex-col w-full pt-5">
             <label>Username</label>
             <input
