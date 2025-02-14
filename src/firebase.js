d57c606850439e9aa82a6703e5fb9565e1bdff13
Dylan Sieren <dylansieren@Dylans-MacBook-Pro.local>
2025-02-14 16:45:30 -0700

    update firebase config to use env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 export const firebaseConfig = {
-    apiKey: "AIzaSyAEQDIakaojdGe5oLjS7HmDoG3ottscx6k",
-    authDomain: "portfolio-39fde.firebaseapp.com",
-    projectId: "portfolio-39fde",
-    storageBucket: "portfolio-39fde.firebasestorage.app",
-    messagingSenderId: "956650241791",
-    appId: "1:956650241791:web:09b1d3681b19e72f6a56ca"
+    apiKey: NEXT_PUBLIC_FIREBASEAPIKEY,
+    authDomain: FIREBASEAUTHDOMAIN,
+    projectId: FIREBASEPID,
+    storageBucket: FIREBASESTORAGEBUCKET,
+    messagingSenderId: FIREBASEMESSAGEID,
+    appId: FIREBASEAPPID
   };
 
   // Initialize Firebase
