e9b5f8d6b2e045946ffc5ae802cd9ca2d0a6a022
Dylan Sieren <dylan.sieren01@gmail.com>
2025-04-30 08:35:40 -0600

    Killed my azure hosting service, needed to set up mock data to have portfolio data live again

--- src/app/components/sections/ProjectSection.tsx
+++ src/app/components/sections/ProjectSection.tsx
@@ -5,42 +5,47 @@ import ProjectCard from "../projects/ProjectCard";
 import axios from "axios";
 import ProjectCardSkeleton from "../projects/ProjectCardSkeleton";
 import SectionHeadingContainer from "../global/SectionHeadingContainer";
+import projectData from "@/data/projectMockData.json";
 
 const ProjectSection = () => {
-  const [projects, setProjects] = useState<ProjectResponseDto[]>([]);
-  const [isFetchingData, setIsFetchingData] = useState<boolean>(true);
+  console.log(projectData.projects);
+  // const [projects, setProjects] = useState<ProjectResponseDto[]>([]);
+  // const [isFetchingData, setIsFetchingData] = useState<boolean>(true);
 
-  const handleFetchingProjects = async () => {
-    try {
-      const response = await axios.get(
-        `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/project`,
-        {
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
-      setProjects(response.data);
-      setIsFetchingData(false);
-    } catch (error) {
-      setIsFetchingData(false);
-      console.error("Error fetching projects:", error);
-    }
-  };
-  useEffect(() => {
-    handleFetchingProjects();
-  }, []);
+  // const handleFetchingProjects = async () => {
+  //   try {
+  //     const response = await axios.get(
+  //       `${process.env.NEXT_PUBLIC_API_BASE_URL}/api/project`,
+  //       {
+  //         headers: {
+  //           "Content-Type": "application/json",
+  //         },
+  //       }
+  //     );
+  //     // setProjects(response.data);
+  //     setIsFetchingData(false);
+  //   } catch (error) {
+  //     setIsFetchingData(false);
+  //     console.error("Error fetching projects:", error);
+  //   }
+  // };
+  // useEffect(() => {
+  //   handleFetchingProjects();
+  // }, []);
 
   return (
     <section id="projects" className="bg-primary py-20">
       <SectionHeadingContainer title={"Featured Work"} />
       <ul className="mx-auto container">
-        {isFetchingData
+        {/* {isFetchingData
           ? [1, 2, 3].map((key: number) => <ProjectCardSkeleton key={key} />)
-          : projects.map((project: ProjectResponseDto) => (
+          : projectData.projects.map((project: ProjectResponseDto) => (
               <ProjectCard key={project.title} {...project} />
-            ))}
-        {}
+            ))} */}
+
+        {projectData.projects.map((project: ProjectResponseDto) => (
+          <ProjectCard key={project.title} {...project} />
+        ))}
       </ul>
     </section>
   );
